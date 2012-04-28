773fb358d66b5bd0c99f1ec0bcf1a8b86d8e5bee
S.Kin <junebongkim@gmail.com>
2012-04-28 10:53:30 +0000

    Add Show

--- requestHandlers.js
+++ requestHandlers.js
@@ -45,7 +45,7 @@ function upload(response, postData) {
 
 function show(response, postData) {
     console.log("Request handler 'show' was called.");
-    fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    fs.readFile("/tmp/pics/nodejs.png", "binary", function(error, file) {
        if (error) {
            response.writeHead(500, {"Content-Type": "text/plain"});
            response.write(error + "\n");
@@ -60,4 +60,4 @@ function show(response, postData) {
 
 exports.start = start;
 exports.upload = upload;
-//exports.show = show;
+exports.show = show;
