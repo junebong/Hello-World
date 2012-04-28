97e8218de9ac6c6e7b7b2dec4a6826698ad99e5c
S.Kin <junebongkim@gmail.com>
2012-04-28 06:27:57 +0000

    First commit from Cloud9 IDE

---
+++ requestHandlers.js
@@ -0,0 +1,63 @@
+/* C9では支持しない　ノンブロッキングの例
+var exec = require("child_process").exec;
+
+function start(response) {
+    console.log("Request handler 'start' was called.");
+    var content = "empty";
+    
+    exec("ls -lah", function (error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+    });
+}*/
+var querystring = require("querystring"),
+    fs = require("fs");
+
+function start(response, postData) {
+    console.log("Request handler 'start' was called.");
+    
+    var body = '<!DOCTPYE html>' +
+        '<html lang="jp">' +
+        '<head>'+
+        '<meta charset="utf-8">' +
+    	'<title>Node-Test</title>' +
+        '</head>' +
+	    '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+		'</body>' +
+        '</html>';
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(body);
+        response.end();
+}
+
+function upload(response, postData) {
+    console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("You've sent:" + 
+    querystring.parse(postData).text);
+    response.end();
+}
+
+function show(response, postData) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("/tmp/test.png", "binary", function(error, file) {
+       if (error) {
+           response.writeHead(500, {"Content-Type": "text/plain"});
+           response.write(error + "\n");
+           response.end();
+       } else {
+           response.writeHead(200, {"Content-Type": "image/png"});
+           response.write(file, "binary");
+           response.end();
+       }
+    });
+}
+
+exports.start = start;
+exports.upload = upload;
+//exports.show = show;
